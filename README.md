23487b714aeccb1bf3b434a8f25d6fea77e701cf
BarRaider <nospam+gh@blachshma.com>
2019-06-29 01:00:59 -0700

    v1.0 - Initial beta

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# streamdeck-textfileupdater
+# Text file updater for the Elgato Stream Deck
+
+Overwrites the contents of a text file with a predefined string. Use to change overlay text during Stream
+
+**Author's website and contact information:** [https://barraider.github.io](https://barraider.github.io)
+
+### Download
+
+* [Download plugin](https://github.com/BarRaider/streamdeck-textfileupdater/releases/
+
+## I found a bug, who do I contact?
+For support please contact the developer. Contact information is available at https://barraider.github.io
+
+## I have a feature request, who do I contact?
+Please contact the developer. Contact information is available at https://barraider.github.io
+
+## Dependencies
+* Uses StreamDeck-Tools by BarRaider: [![NuGet](https://img.shields.io/nuget/v/streamdeck-tools.svg?style=flat)](https://www.nuget.org/packages/streamdeck-tools)
+* Uses [Easy-PI](https://github.com/BarRaider/streamdeck-easypi) by BarRaider - Provides seamless integration with the Stream Deck PI (Property Inspector) 
---
+++ streamdeck-textfileupdater/TextFileUpdaterAction.cs
@@ -0,0 +1,105 @@
+﻿using BarRaider.SdTools;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarRaider.TextFileUpdater
+{
+    [PluginActionId("com.barraider.textfileupdater")]
+    public class TextFileUpdaterAction : PluginBase
+    {
+        private class PluginSettings
+        {
+            public static PluginSettings CreateDefaultSettings()
+            {
+                PluginSettings instance = new PluginSettings();
+                instance.OutputFileName = String.Empty;
+                instance.InputString = String.Empty;
+                return instance;
+            }
+
+            [FilenameProperty]
+            [JsonProperty(PropertyName = "outputFileName")]
+            public string OutputFileName { get; set; }
+
+            [JsonProperty(PropertyName = "inputString")]
+            public string InputString { get; set; }
+        }
+
+        #region Private Members
+
+        private PluginSettings settings;
+
+        #endregion
+        public TextFileUpdaterAction(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            if (payload.Settings == null || payload.Settings.Count == 0)
+            {
+                this.settings = PluginSettings.CreateDefaultSettings();
+            }
+            else
+            {
+                this.settings = payload.Settings.ToObject<PluginSettings>();
+            }
+        }
+
+        public override void Dispose()
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
+        }
+
+        public override void KeyPressed(KeyPayload payload)
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, "Key Pressed");
+            SaveInputStringToFile();
+        }
+
+        public override void KeyReleased(KeyPayload payload) { }
+
+        public override void OnTick() { }
+
+        public override void ReceivedSettings(ReceivedSettingsPayload payload)
+        {
+            Tools.AutoPopulateSettings(settings, payload.Settings);
+            SaveSettings();
+        }
+
+        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+        #region Private Methods
+
+       
+        private void SaveInputStringToFile()
+        {
+            try
+            {
+                Logger.Instance.LogMessage(TracingLevel.INFO, $"Saving value: {settings.InputString} to file: {settings.OutputFileName}");
+                if (!String.IsNullOrWhiteSpace(settings.OutputFileName))
+                {
+
+                    File.WriteAllText(settings.OutputFileName, settings.InputString);
+                    Connection.ShowOk();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, $"Error Saving value: {settings.InputString} to file: {settings.OutputFileName} : {ex}");
+                Connection.ShowAlert();
+                settings.OutputFileName = "ACCESS DENIED";
+                SaveSettings();
+            }
+        }
+
+        private Task SaveSettings()
+        {
+            return Connection.SetSettingsAsync(JObject.FromObject(settings));
+        }
+
+       #endregion
+    }
+}
